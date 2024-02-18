fe9839ebe6f5e7e917e4a77537c80e12553334cf
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-02-18 10:36:36 +0600

    :tada: init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { cn } from "@/lib/utils"
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const fontSans = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}>{children}</body>
     </html>
   );
 }
