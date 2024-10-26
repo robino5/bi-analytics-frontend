0800fc33fe62d506ea1613cbb6869ce851fb2371
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-10-26 18:22:55 +0600

    fix(misc): fixing misc issues

--- app/dashboard/rm/daily-trade-performance/page.tsx
+++ app/dashboard/rm/daily-trade-performance/page.tsx
@@ -387,7 +387,7 @@ export default function DailyTradePerformance() {
             className="col-span-6 xl:col-span-2"
             title="Summary"
             subtitle="shows overall short summary"
-            boardIcon={<PiChartScatterBold className="h-7 w-7 text-gray-400"/>}
+            boardIcon={<PiChartScatterBold className="h-7 w-7 text-gray-400" />}
             children={
               <StatisticsCardClientTurnoverSummary
                 data={summary.shortSummary}
@@ -402,7 +402,7 @@ export default function DailyTradePerformance() {
             className="col-span-6 xl:col-span-2"
             title="Cash Code Status"
             subtitle="shows cash code summary"
-            boardIcon={<FaChartSimple className="h-7 w-7 text-gray-400"/>}
+            boardIcon={<FaChartSimple className="h-7 w-7 text-gray-400" />}
             children={
               <StatisticsCashCodeSummary data={summary.cashCodeSummary} />
             }
@@ -415,7 +415,7 @@ export default function DailyTradePerformance() {
             className="col-span-6 xl:col-span-2"
             title="Margin Code Status"
             subtitle="shows margin code summary"
-            boardIcon={<IoPieChartSharp className="h-7 w-7 text-gray-400"/>}
+            boardIcon={<IoPieChartSharp className="h-7 w-7 text-gray-400" />}
             children={
               <StatisticsMarginCodeSummary data={summary.marginCodeSummary} />
             }
@@ -427,8 +427,8 @@ export default function DailyTradePerformance() {
         {turnoverPerformance ? (
           <CardBoard
             className="col-span-6 xl:col-span-3"
-            title={"Turnover Performance"}
-            subtitle="Shows a analytics of turnover performance of last 7 days."
+            title={"Daily Turnover Target"}
+            subtitle="Shows a analytics of turnover target performance of last 7 days."
             children={
               <BarChartVerticalGrouped
                 data={turnoverPerformance}
