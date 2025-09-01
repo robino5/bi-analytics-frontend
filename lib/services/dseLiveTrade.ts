b86e9e193bc72f26e86aec72cf58f239dcf9a0c9
Anamul Haque Robin <anamul.robin@lbsbd.com>
2025-09-01 11:20:56 +0600

    add no data found in active trading coe

--- lib/services/dseLiveTrade.ts
+++ lib/services/dseLiveTrade.ts
@@ -2,24 +2,27 @@ import config from "@/config";
 import { Common } from "@/lib/api/common";
 import { HttpAuthService } from "@/lib/httpService";
 import { authService } from "@/lib/auth";
-import {DseLiveDsex, DseLiveTrade } from "@/types/dseLiveTradeType";
+import { DseLiveDsex, DseLiveTrade } from "@/types/dseLiveTradeType";
 
 
 class DseLiveTradeAPI extends Common {
     constructor(private http: HttpAuthService) {
         super()
     }
-   
+
     getDseLiveTradeData() {
         return this.http.get<DseLiveTrade>("/dashboards/portal-dse-live-trade/");
     }
 
-     getDseLiveDsexData() {
+    getDseLiveDsexData() {
         return this.http.get<DseLiveDsex[]>("/dashboards/portal-live-dse-dsex/");
     }
 
     getDseLiveDsexSumData() {
         return this.http.get<[]>("/dashboards/portal-live-dse-dsex-summary/");
+    }
+    getLiveMarketSentiment() {
+        return this.http.get<number>("dashboards/live-market-sentiment/");
     }
 }
 
