2904d0df754d5c1a4fd276e41c8749d5e78e5513
robino5 <anamulrobin05@gmail.com>
2026-05-18 13:56:20 +0600

    call region wise data

---
+++ app/dashboard/active-trading-codes/_components/_regin_wise_client_chart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ChannelTradingRecord } from "../types";
+import * as echarts from "echarts";
+
+const RegionWiseClientChart = (date: ChannelTradingRecord) => {
+
+  return (
+    <div> RegionWiseClientChart</div>
+  );
+}
+export default RegionWiseClientChart;
